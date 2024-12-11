cac7e9d471d0c4cbe534da7c662228ec6779c519
root <root@Elyes.>
2024-12-11 13:54:27 +0100

    binaries

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,16 +5,17 @@
  * @tree: A pointer to the root node of the tree to measure the height.
  * Return: The height of the tree.
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height, right_height;
+size_t left_height = 0;
+size_t right_height = 0;
+size_t max_height = 0;
 if (tree == NULL)
 return (0);
+if (tree->left != NULL)
 left_height = binary_tree_height(tree->left);
+if (tree->right != NULL)
 right_height = binary_tree_height(tree->right);
-if (left_height > right_height)
-return (left_height + 1);
-else
-return (right_height + 1);
+max_height = (left_height > right_height) ? left_height : right_height;
+return (max_height + 1);
 }
